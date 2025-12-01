d97610b5966526c326f4675a01ea1d64bdce3b09
ShadowsAdi <54354938+ShadowsAdi@users.noreply.github.com>
2025-12-01 23:27:47 +0200

    Complete `migrations` and fixed `DatabaseSeeder`

---
+++ ProiectIS2/Database/Migrations/20251201211229_AddCategory.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ProiectIS2.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "CatFacts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "General" },
+                    { 2, "Science" },
+                    { 3, "Funny" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CatFacts_CategoryId",
+                table: "CatFacts",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CatFacts_Categories_CategoryId",
+                table: "CatFacts",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CatFacts_Categories_CategoryId",
+                table: "CatFacts");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CatFacts_CategoryId",
+                table: "CatFacts");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "CatFacts");
+        }
+    }
+}
